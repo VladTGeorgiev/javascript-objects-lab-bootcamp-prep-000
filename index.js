110e56cdeb4a9c5431cfc08a5dd4defc1976fd51
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-18 22:05:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = object.assign({}, {key: value})
+  var newObject = object.assign({keyNew: valueNew}, {key: value})
   return newObject
 }
